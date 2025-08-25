2c1abdbb4a6dcc52bfe40a2bd22a10cd421f3837
dinosaur890123 <dinosaur890123@gmail.com>
2025-08-25 13:39:24 +1000

    rearranged js

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
-    setupWaterLogger();
-    setupWorkoutLogger()
-});
-function setupWaterLogger() {
     const logWaterButton = document.getElementById('log-water-button');
     const waterCountSpan = document.getElementById('water-count');
+    const logWorkoutButton = document.getElementById('log-workout-button')
+    const workoutInput = document.getElementById('workout-input');
+    const workoutList = document.getElementById('workout-list');
+    if (!logWaterBtn || !waterCountSpan || !logWorkoutBtn || !workoutInput || !workoutList) {
+        console.error("An element was not found") 
+        return;
+    }
+    setupWaterLogger(logWaterButton, waterCountSpan);
+    setupWorkoutLogger(logWorkoutButton, workoutInput, workoutList)
+});
+function setupWaterLogger(button, display) {
     let waterCount = 0;
     logWaterButton.addEventListener('click', () => {
         waterCount++;
         waterCountSpan.textContent = waterCount;
     })
 }
-function setupWorkoutLogger() {
-    const logWorkoutButton = document.getElementById('log-workout-button')
-    const workoutInput = document.getElementById('workout-input');
-    const workoutList = document.getElementById('workout-list');
+function setupWorkoutLogger(button, input, list) {
     logWorkoutButton.addEventListener('click', () => {
         const workoutText = workoutInput.value.trim();
         if (workoutText !== "") {
             addWorkoutToList(workoutText)
-            workoutInput.value = "";
+            input.value = "";
         }});
 }
 function addWorkoutToList(text) {
-    const workoutList = document.getElementById('workout-list');
     const newListItem = document.getElementById('li');
     newListItem.textContent = text;
-    workoutList.appendChild(newListItem);
+    list.appendChild(newListItem);
 }
