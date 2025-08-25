9f33d27c8b32634f01d89b7364f10c3f8949d13f
dinosaur890123 <dinosaur890123@gmail.com>
2025-08-25 21:02:54 +1000

    added water goal logging

--- script.js
+++ script.js
@@ -4,14 +4,33 @@ document.addEventListener('DOMContentLoaded', () => {
     const logWorkoutButton = document.getElementById('log-workout-button')
     const workoutInput = document.getElementById('workout-input');
     const workoutList = document.getElementById('workout-list');
+    const waterGoalInput = document.getElementById('water-goal-input');
+    const setGoalButton = document.getElementById('set-goal-button');
+    const waterGoalSpan = document.getElementById('water-goal');
     setupWaterLogger(logWaterButton, waterCountSpan);
     setupWorkoutLogger(logWorkoutButton, workoutInput, workoutList);
 });
-function setupWaterLogger(button, display) {
+function setupWaterLogger(logButton, countDisplay, goalDisplay, goalInput, setGoalButton) {
     let waterCount = 0;
-    button.addEventListener('click', () => {
+    let waterGoal = 0;
+    function updateDisplay() {
+        countDisplay.textContent = waterCount;
+        goalDisplay.textContent = waterGoal;
+        if (waterCount >= waterGoal) {
+            console.log("You reached your goal");
+        }
+    }
+    setGoalButton.addEventListener('click', () => {
+        const newGoal = parseInt(goalInput.value);
+        if (newGoal && newGoal > 0) {
+            waterGoal = newGoal;
+            updateDisplay();
+            goalInput.value = '';
+        }
+    })
+    logButton.addEventListener('click', () => {
         waterCount++;
-        display.textContent = waterCount;
+        updateDisplay();       
     })
 }
 function setupWorkoutLogger(button, input, list) {
